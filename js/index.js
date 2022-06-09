29f6c67012c66bca88a606499b4dd61fe0b500a7
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-06-08 21:43:31 -0300

    remove header

--- js/index.js
+++ js/index.js
@@ -11,19 +11,19 @@ function isElementInViewport(el) {
     rect.top >= 0 &&
     rect.left >= 0 &&
     rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+    (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
 function callbackFunc() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("#header").style.top = "0";
-  } else {
-    document.querySelector("#header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+  // let currentScrollPos = window.pageYOffset;
+  // if (prevScrollpos > currentScrollPos) {
+  //   document.querySelector("#header").style.top = "0";
+  // } else {
+  //   document.querySelector("#header").style.top = "-50px";
+  // }
+  // prevScrollpos = currentScrollPos;
   for (let i = 0; i < items.length; i++) {
     if (isElementInViewport(items[i])) {
       items[i].classList.add("in-view");
